b205eec839d934eed6ddd0091e275b1baff28918
ixotoxin <vitaly@anasenko.com>
2025-08-21 01:54:31 +0300

    Minor improvements.

--- _clang/install_deps.cmd
+++ _clang/install_deps.cmd
@@ -1,10 +1,3 @@
 @ECHO OFF
-CLS
-CALL "%~dp0config_env.cmd"
-IF NOT EXIST "%~dp0..\deps\fptr10" (
-    MKDIR "%~dp0..\deps\fptr10"
-    XCOPY /S/E/H "%programfiles%\ATOL\Drivers10\KKT\langs\cpp\fptr10" "%~dp0..\deps\fptr10"
-)
-CALL %VCPKG_DIR%\bootstrap-vcpkg.bat
 @REM TODO: Реализовать компиляцию зависимостей clang'ом
-%VCPKG_BIN% --x-manifest-root=%~dp0..\deps install
+CALL "%~dp0..\_msvc\install_deps.cmd"
--- _msvc/install_deps.cmd
+++ _msvc/install_deps.cmd
@@ -5,5 +5,5 @@ IF NOT EXIST "%~dp0..\deps\fptr10" (
     MKDIR "%~dp0..\deps\fptr10"
     XCOPY /S/E/H "%programfiles%\ATOL\Drivers10\KKT\langs\cpp\fptr10" "%~dp0..\deps\fptr10"
 )
-CALL %VCPKG_DIR%\bootstrap-vcpkg.bat
-%VCPKG_DIR%\vcpkg --x-manifest-root=%~dp0..\deps install
+CALL %VCPKG_DIR%\bootstrap-vcpkg.bat -disableMetrics
+%VCPKG_DIR%\vcpkg --disable-metrics --vcpkg-root=%VCPKG_DIR% --x-manifest-root=%~dp0..\deps install
