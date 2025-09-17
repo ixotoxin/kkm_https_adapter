b2d0c092701f507545d6df9c98e93ab973b7e851
ixotoxin <vitaly@anasenko.com>
2025-09-18 00:55:57 +0300

    Sanitizers integration initialized.

--- _clang/config_env.cmd
+++ _clang/config_env.cmd
@@ -1,7 +1,8 @@
 @ECHO OFF
 IF NOT "%CLANG_DIR%" == "" GOTO CLANG_DIR_IS_SET
 SET CLANG_DIR=C:\Devel\Platform\Clang\21.1.1-x86_64
-SET CLANG_XSAN_LIB_DIR=%CLANG_DIR%\lib\clang\21\lib\windows
+SET CLANG_SANLIB_DIR=%CLANG_DIR%\lib\clang\21\lib\windows
+SET CLANG_ASAN_LIB=clang_rt.asan_dynamic-x86_64.dll
 SET CMAKE_DIR=C:\Devel\Platform\CMake\3.31.7-x86_64
 SET NINJA_DIR=C:\Devel\Platform\Ninja\1.13.1
 SET VCPKG_DIR=C:\Devel\Platform\vcpkg
