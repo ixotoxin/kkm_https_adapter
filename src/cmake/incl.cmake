d18c981d886929edbfc36282652bcbbe1284d6d0
ixotoxin <vitaly@anasenko.com>
2025-10-20 00:55:11 +0300

    CMakeLists.txt refactored.

--- src/cmake/incl.cmake
+++
@@ -1,6 +0,0 @@
-set(EXPORT_CONFIG_INCL "${PROJECT_BINARY_DIR}/_Include_${CMAKE_BUILD_TYPE}")
-
-configure_file("${PROJECT_SOURCE_DIR}/src/cmake/options.h.in" "${EXPORT_CONFIG_INCL}/cmake/options.h")
-configure_file("${PROJECT_SOURCE_DIR}/src/cmake/variables.h.in" "${EXPORT_CONFIG_INCL}/cmake/variables.h")
-
-include_directories("${EXPORT_CONFIG_INCL}" BEFORE)
--- src/library/incl.cmake
+++
@@ -1,7 +0,0 @@
-file(GLOB_RECURSE LIB_SOURCE_FILES "${PROJECT_SOURCE_DIR}/src/library/lib/*.cpp")
-file(GLOB_RECURSE MAIN_SOURCE_FILES "${PROJECT_SOURCE_DIR}/src/library/main/*.cpp")
-file(GLOB_RECURSE LOG_SOURCE_FILES "${PROJECT_SOURCE_DIR}/src/library/log/*.cpp")
-file(GLOB_RECURSE KKM_SOURCE_FILES "${PROJECT_SOURCE_DIR}/src/library/kkm/*.cpp")
-file(GLOB_RECURSE CONF_SOURCE_FILES "${PROJECT_SOURCE_DIR}/src/library/config/*.cpp")
-
-include_directories("${PROJECT_SOURCE_DIR}/src/library" BEFORE)
